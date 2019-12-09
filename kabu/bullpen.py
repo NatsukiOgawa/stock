39c5e2ba3b582363957cbd0bc1671bc8ddd935bd
NatsukiOgawa <ogawanatsuki19960701@gmail.com>
2019-12-09 12:44:56 +0900

    dec/9/2019/12:44/mac_ogawa

--- kabu/bullpen.py
+++
@@ -1,7 +0,0 @@
-import Main
-
-print("bullpen !!!")
-for year in range(2000, 2020):
-    xxx = Main.kabuka_class()
-    kabuka_data = xxx.kabuka("https://kabuoji3.com/stock/6501/{}/".format(str(year)))
-print("FINISH !!!!!")
