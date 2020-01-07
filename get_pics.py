1e83e3c1dd2f9ba9da15464e7196c343b1150e6d
NatsukiOgawa <ogawanatsuki19960701@gmail.com>
2020-01-07 11:59:06 +0900

    janu/7/2020/11:57/ubutn_labo

--- get_pics.py
+++ get_pics.py
@@ -1,79 +1,64 @@
+import bs4
 import requests
-from bs4 import BeautifulSoup
-from urllib.parse import urljoin
-import urllib.request
+import re
+import urllib.request, urllib.error
 import os
+import argparse
+import sys
+import json
 
+# How To
+# (1) ソースコードを保存して命名する (e.g. scrape.py)
+# (2) プログラムを起動する python scrape.py
+# (3) オプション
+# -s: Google Imagesにかける検索キーワード、複数可 (デフォルト "banana")
+# -n: ダウンロードする画像の数量 (デフォルト 10枚)
+# -o: 画像の保存先 (デフォルト　<DEFAULT_SAVE_DIRECTORY>で指定する)
+def get_soup(url,header):
+    return bs4.BeautifulSoup(urllib.request.urlopen(urllib.request.Request(url,headers=header)),'html.parser')
 
-folder= './figures'
-# 保存先フォルダ名
+def main(args):
+    parser = argparse.ArgumentParser(description='Options for scraping Google images')
+    parser.add_argument('-s', '--search', default='banana', type=str, help='search term')
+    parser.add_argument('-n', '--num_images', default=10, type=int, help='num of images to scrape')
+    parser.add_argument('-o', '--directory', default='./figures', type=str, help='output directory')
+    args = parser.parse_args()
 
-os.makedirs(folder, exist_ok=True)
-# フォルダ名でディレクトリを作成する
+    # 複数のキーワードを"+"で繋げる
+    query = args.search.split()
+    query = '+'.join(query)
+    max_images = args.num_images
 
-url_list= []
-title_list=[]
+    # 画像をフォルダーでグループする
+    save_directory = args.directory + '/' + query
+    if not os.path.exists(save_directory):
+        os.makedirs(save_directory)
 
-url_url = "https://www.google.com/search?q=%E3%82%8A%E3%82%93%E3%81%94&sxsrf=ACYBGNQSQombi7OznFdW86BbQTX4qUP2hg:1578292629354&source=lnms&tbm=isch&sa=X&ved=2ahUKEwi1wJicru7mAhUnG6YKHT3XD5gQ_AUoAXoECBIQAw&biw=1690&bih=975#imgrc=_"
-url_url = "https://www.google.com/search?q=%E5%9B%BD%E6%97%97&sxsrf=ACYBGNQvT69ycxqTi1bMVg-ZBYPlWn-0Hg:1578299085327&source=lnms&tbm=isch&sa=X&ved=2ahUKEwir29Gixu7mAhUExIsBHfb-CSwQ_AUoAXoECBAQAw&biw=1690&bih=975&dpr=1.1"
-# スクレイピングするURL
+    # スクレーピング
+    url="https://www.google.co.jp/search?q="+query+"&source=lnms&tbm=isch"
+    header={'User-Agent':"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36"}
+    soup = get_soup(url,header)
+    ActualImages=[]
 
-def get_hp(url_url):
-	res = requests.get(url_url)
-	res.raise_for_status() #エラーならここで例外を発生させる
-	return res.text
+    for a in soup.find_all("div",{"class":"rg_meta"}):
+        link , Type =json.loads(a.text)["ou"]  ,json.loads(a.text)["ity"]
+        ActualImages.append((link,Type))
+    for i , (img , Type) in enumerate( ActualImages[0:max_images]):
+        try:
+            Type = Type if len(Type) > 0 else 'jpg'
+            print("Downloading image {} ({}), type is {}".format(i, img, Type))
+            raw_img = urllib.request.urlopen(img).read()
+            f = open(os.path.join(save_directory , "img_"+str(i)+"."+Type), 'wb')
+            f.write(raw_img)
+            f.close()
+        except Exception as e:
+            print ("could not load : "+img)
+            print (e)
 
-
-def pickup_tag(url_url):
-	soup = BeautifulSoup(get_hp(url_url), 'html.parser')
-	# get_tag = soup.select("img.thumbnail")
-	get_tag = soup.select("img")
-	#get_tag = list(dict.fromkeys(get_tag))
-	if get_tag is None:
-		print("見つかりません")
-		quit()
-	return get_tag
-
-print("element :", len(pickup_tag(url_url)))
-
-
-def get_photo_url(url_url):
-	for temp in pickup_tag(url_url):
-		url_fact = temp.get("src")
-		yield url_fact
-		url_list.append(url_fact)
-
-
-def get_title(url_url):
-	for i in get_photo_url(url_url):
-		pass
-
-	soup = BeautifulSoup(get_hp(url_url), 'html.parser')
-	# get_title_tag = soup.select('h2 a')
-	get_title_tag = soup.select('img')
-	if get_title_tag is None:
-		print('見つかりません')
-		quit()
-	for sample in get_title_tag:
-		element_titile = sample.get_text()
-		yield element_titile
-		title_list.append(element_titile)
-
-for i in get_title(url_url):
-	pass
-
-print(len(title_list))
-# """
-for n in range(len(url_list)):
-	# image_url = "https://www.google.com/?hl=ja" + url_list[n]
-	image_url = url_list[n]
-	print('画像をダウンロード中 {}...'.format(image_url))
-	res = requests.get(image_url)
-	res.raise_for_status()
-	# image_file = open(os.path.join(folder, os.path.basename(title_list[n]+'.jpg')), 'wb')
-	image_file = open(os.path.join(folder, os.path.basename(str(n) +'.jpg')), 'wb')
-	# for chunk in res.iter_content(100000):
-	for chunk in res.iter_content(100000):
-		image_file.write(chunk)
-	image_file.close()
-# """
+if __name__ == '__main__':
+    from sys import argv
+    try:
+        main(argv)
+    except KeyboardInterrupt:
+        pass
+    sys.exit()
