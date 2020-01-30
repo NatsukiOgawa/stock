523c70cbef64a8141f761ef94950744050116b2b
NatsukiOgawa <ogawanatsuki19960701@gmail.com>
2020-01-30 17:00:02 +0900

    2020年01月30日17時00分02秒

--- get_pics.py
+++ get_pics.py
@@ -1,3 +1,6 @@
+import os
+import time
+import datetime
 from tkinter import messagebox
 import bs4
 import requests
@@ -58,6 +61,21 @@ class get_soup_class ():
                 print ("could not load : "+img)
                 print (e)
 
+class file_name_dates_class():
+    def file_name_dates(self):
+        now = datetime.datetime.now()
+        now_id = now
+        now_id = str(now_id)
+
+        id = ""
+        for i in range(20, 26):
+            id += str(now_id[i])
+        file_name = now.strftime('%Y' + "年" + '%m' + "月" + '%d' + "日" + '%H' + "時" + '%M' + "分" + '%S' + "秒")
+        no_mili = file_name
+        file_name += id
+        # print(file_name)
+        return file_name, no_mili
+
 if __name__ == '__main__':
 
     # ['get_pics.py', '-n', '3', '-s', 'no']
@@ -108,6 +126,14 @@ if __name__ == '__main__':
         print()
         print()
         print()
+
+    aaa = file_name_dates_class()
+    aaa_aaa = aaa.file_name_dates()
+    os.system('git add *')
+    os.system('git commit -m "{}"'.format(aaa_aaa[1]))
+    os.system('git push')
+    os.system('rm -rf figures')
+
     messagebox.showinfo('通知', '全作業が終了しました.')
     sys.exit()
     # """
