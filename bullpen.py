35a0dd118a131a8783cf30ebf79cf5f42a94fb80
NatsukiOgawa <ogawanatsuki19960701@gmail.com>
2020-01-30 16:28:09 +0900

    1580369289.6341171

--- bullpen.py
+++ bullpen.py
@@ -1,6 +1,10 @@
 import os
+import time
 
+now = time.time()
 # os.system('git add *')
 # os.system('git commit -m "test commit"')
+os.system('git add *')
+os.system('git commit -m "{}"'.format(now))
 os.system('git push')
-os.system('NatsukiOgawa')
+# os.system('git push')
